1c65b4591c6da56d998a63681834061a34648d07
Bruno Costa <bruno.costa@superlogica.com>
2020-10-27 13:01:34 -0300

    arrumando o cadastro

--- app/Services/UserService.php
+++ app/Services/UserService.php
@@ -2,12 +2,23 @@
 
 namespace App\Services;
 
+use App\Models\User;
 use BRCas\User\Services\UserService as ServicesUserService;
+use Illuminate\Support\Facades\Hash;
 
 class UserService extends ServicesUserService  {
     private function registerPermissions($obj, array $permissions)
     {
         $obj->syncPermissions($permissions);
+    }
+
+    public function create(array $data)
+    {
+        $data['password'] = Hash::make($data['password']);
+        $obj = User::create($data);
+        $this->registerPermissions($obj, $data['permissions'] ?? []);
+
+        return $obj;
     }
 
     public function edit($obj, $data){
