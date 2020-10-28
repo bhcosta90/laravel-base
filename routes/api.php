fc709de7cc27b90c732793f2642df102f8eaa464
Bruno Costa <bruno.costa@superlogica.com>
2020-10-28 11:26:55 -0300

    removendo company

--- routes/api.php
+++ routes/api.php
@@ -14,7 +14,3 @@
 // Route::middleware('auth:api')->get('/user', function (Request $request) {
 //     return $request->user();
 // });
-
-use App\Http\Controllers\Api\Admin\CompanyController;
-
-Route::resource('company', CompanyController::class);
