f9dfddcafbf566f840bd28f01cbaa540647baccd
Bruno Costa <bruno.costa@superlogica.com>
2020-10-28 20:30:40 -0300

    colocando profile

--- resources/views/vendor/adminlte/partials/navbar/menu-item-dropdown-user-menu.blade.php
+++ resources/views/vendor/adminlte/partials/navbar/menu-item-dropdown-user-menu.blade.php
@@ -1,5 +1,5 @@
 @php( $logout_url = View::getSection('logout_url') ?? config('adminlte.logout_url', 'logout') )
-@php( $profile_url = View::getSection('profile_url') ?? config('adminlte.profile_url', 'logout') )
+@php( $profile_url = View::getSection('profile_url') ?? Route::has('profile') ? route('profile') : '' )
 
 @if (config('adminlte.usermenu_profile_url', false))
     @php( $profile_url = Auth::user()->adminlte_profile_url() )
