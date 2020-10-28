6d88eff20f9d71172e03c776477149e9bc774902
Bruno Costa <bruno.costa@superlogica.com>
2020-10-28 10:04:16 -0300

    retirando classe nao utilizada

--- database/migrations/2020_10_27_122104_add_permission_1.php
+++ database/migrations/2020_10_27_122104_add_permission_1.php
@@ -2,8 +2,6 @@
 
 use App\Traits\Permission;
 use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
-use Illuminate\Support\Facades\Schema;
 
 class AddPermission1 extends Migration
 {
