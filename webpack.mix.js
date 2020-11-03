3e883b79cce55d6fda70f700a83c2abbcc6c6f13
Bruno Costa <bruno.costa@superlogica.com>
2020-11-03 12:05:39 -0300

    webpack

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,5 +12,6 @@ const mix = require('laravel-mix');
  */
 
 mix.js('resources/js/app.js', 'public/js')
+    .scripts('resources/js/adminlte.js', 'public/js/adminlte.js')
     .sass('resources/sass/app.scss', 'public/css')
     .sass('resources/sass/adminlte.scss', 'public/css');
