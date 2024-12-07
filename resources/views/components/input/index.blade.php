6a7ce57e5d18bf425d7e43c40f0de9c93838b230
Bruno Costa <bhcosta90@gmail.com>
2024-12-07 16:02:14 -0300

    LB-01: add label

--- resources/views/components/input/index.blade.php
+++ resources/views/components/input/index.blade.php
@@ -1,1 +1,12 @@
-<input {{ $attributes }} />
+@props(['label' => null])
+
+<span>
+    @if($label)
+        <label class="block text-sm font-medium text-gray-700">
+        {{ $label }}
+    @endif
+    <input {{ $attributes }} />
+    @if($label)
+        </label>
+    @endif
+</span>
