2f3b7c09632941381bf4971c035a9a4b259af58e
Bruno Costa <bhcosta90@gmail.com>
2024-12-07 14:50:21 -0300

    LB-01: formatter auth

--- routes/auth.php
+++ routes/auth.php
@@ -1,7 +1,9 @@
 <?php
 
+declare(strict_types = 1);
+
 use Illuminate\Support\Facades\Route;
 
-Route::middleware('auth')->group(function(){
-    Route::get('validation', fn() => 'oi')->name('auth.email-validation');
+Route::middleware('auth')->group(function () {
+    Route::get('validation', fn () => 'oi')->name('auth.email-validation');
 });
