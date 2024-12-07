b0a7c2041298557d1e3e1c9d3f3b4657e8044ceb
Bruno Costa <bhcosta90@gmail.com>
2024-12-07 15:18:02 -0300

    LB-02: change component

--- routes/auth.php
+++ routes/auth.php
@@ -2,8 +2,9 @@
 
 declare(strict_types = 1);
 
+use App\Livewire\Auth;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth')->group(function () {
-    Route::get('validation', fn () => 'oi')->name('auth.email-validation');
+    Route::get('validation', Auth\EmailValidation::class)->name('auth.email-validation');
 });
