889723171c80608b396d2c7e07f0addf37d8dd85
Bruno Costa <bhcosta90@gmail.com>
2025-03-01 12:42:23 -0300

    [BS-3] feat: implement rate limiting and validation in login component

--- app/Livewire/Auth/Login.php
+++ app/Livewire/Auth/Login.php
@@ -7,9 +7,12 @@ namespace App\Livewire\Auth;
 use App\Models\User;
 use App\Notifications\User\TokenNotification;
 use Illuminate\Contracts\View\View;
-use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\{Auth, RateLimiter};
+use Illuminate\Support\Str;
+use Livewire\Attributes\Layout;
 use Livewire\Component;
 
+#[Layout('components.layouts.guest')]
 class Login extends Component
 {
     public ?string $login = '';
@@ -27,26 +30,61 @@ class Login extends Component
 
     public function submit(): void
     {
+        if ($this->ensureIsNotRateLimiting(__FUNCTION__)) {
+            return;
+        }
+
+        $this->validate([
+            'login' => 'required|string',
+        ]);
+
         if (filter_var($this->login, FILTER_VALIDATE_EMAIL)) {
             $this->isEmail = true;
 
             $user = User::whereLogin($this->login)->first();
 
-            $user->password = $password = mb_strtoupper(str()->random(6));
-            $user->save();
+            if ($user) {
+                $user->password = $password = mb_strtoupper(str()->random(6));
+                $user->save();
 
-            $user->notify(new TokenNotification($password));
+                $user->notify(new TokenNotification($password));
+            }
         } else {
             $this->isLogin = true;
         }
+
+        RateLimiter::hit($this->throttleKey(__FUNCTION__));
     }
 
-    public function login(): void
+    public function execute(): void
     {
+        if ($this->ensureIsNotRateLimiting(__FUNCTION__)) {
+            return;
+        }
+
         if (Auth::attempt(['login' => $this->login, 'password' => $this->password])) {
             $this->redirect('dashboard');
         }
 
+        RateLimiter::hit($this->throttleKey(__FUNCTION__));
         $this->addError('login', __('auth.failed'));
+    }
+
+    protected function throttleKey(string $action): string
+    {
+        return Str::transliterate(Str::lower($action . $this->login) . '|' . request()->ip());
+    }
+
+    protected function ensureIsNotRateLimiting(string $action): bool
+    {
+        if (RateLimiter::tooManyAttempts($this->throttleKey($action), 5)) {
+            $this->addError('rateLimiter', trans('auth.throttle', [
+                'seconds' => RateLimiter::availableIn($this->throttleKey($action)),
+            ]));
+
+            return true;
+        }
+
+        return false;
     }
 }
