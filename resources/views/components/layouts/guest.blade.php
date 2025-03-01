a300eabd9c964237fa2f6dd7f0caa6d8d42f3967
Bruno Costa <bhcosta90@gmail.com>
2025-03-01 11:24:07 -0300

    [BS-3] feat: implement new login UI with Livewire components and enhance password reset tests

---
+++ resources/views/components/layouts/guest.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+        <title>{{ $title ?? 'Page Title' }}</title>
+        @if (file_exists(public_path('build/manifest.json')) || file_exists(public_path('hot')))
+            @vite(['resources/css/app.css', 'resources/js/app.js'])
+        @endif
+        @livewireStyles
+    </head>
+    <body data-theme="admin" class="flex flex-col font-sans antialiased min-h-full max-w-screen bg-neutral-content">
+        <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0">
+            <div class="w-full sm:max-w-md overflow-hidden sm:rounded-lg">
+                {{ $slot }}
+            </div>
+        </div>
+        @livewireScripts
+    </body>
+</html>
