d6a4e3de4bcb5686f19711ffa24f2f726abd9726
Bruno Costa <bhcosta90@gmail.com>
2025-03-01 11:12:13 -0300

    [BS-3] feat: add Notifiable trait to User model for notification capabilities

--- app/Models/User.php
+++ app/Models/User.php
@@ -8,11 +8,13 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
     use HasFactory;
     use SoftDeletes;
+    use Notifiable;
 
     protected $fillable = [
         'name',
