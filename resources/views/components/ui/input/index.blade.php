a324cbcab3bb92aa4d737c0f6dcdf1a29750f82f
Bruno Costa <bhcosta90@gmail.com>
2025-03-01 12:42:10 -0300

    [BS-3] feat: add error display option to input component in index.blade.php

--- resources/views/components/ui/input/index.blade.php
+++ resources/views/components/ui/input/index.blade.php
@@ -1,14 +1,31 @@
-@props(['label' => null, 'placeholder' => false])
+@props([
+    'label' => null,
+    'placeholder' => false,
+    'showError' => true
+])
+
+@php
+    $name ??= $attributes->wire('model')->value();
+    $id = $name ?? uniqid('', true);
+@endphp
 
 <div>
     @if($label)
         <label class="block text-sm">
-        <div class="mb-0.5">{{ __($label) }}</div>
-    @endif
-    <input placeholder="{{ __($placeholder) }}" {{ $attributes->class([
+            <div class="mb-0.5">{{ __($label) }}</div>
+            @endif
+            <input placeholder="{{ __($placeholder) }}" {{ $attributes->class([
         'input input-bordered w-full'
     ]) }} />
-    @if($label)
+            @if($label)
         </label>
+    @endif
+
+    @if ($showError)
+        @error( $name )
+        <label class="label">
+            <span class="label-alt text-error text-sm">{{ $message }}</span>
+        </label>
+        @enderror
     @endif
 </div>
