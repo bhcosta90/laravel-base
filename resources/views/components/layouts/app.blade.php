f87d89ff2794f4c5c99b43eac86269059d53e3b9
Bruno Costa <bhcosta90@gmail.com>
2025-03-01 12:41:58 -0300

    [BS-3] feat: add Livewire styles and scripts to main layout for component support

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -8,8 +8,10 @@
         @if (file_exists(public_path('build/manifest.json')) || file_exists(public_path('hot')))
             @vite(['resources/css/app.css', 'resources/js/app.js'])
         @endif
+        @livewireStyles
     </head>
     <body>
         {{ $slot }}
+        @livewireScripts
     </body>
 </html>
