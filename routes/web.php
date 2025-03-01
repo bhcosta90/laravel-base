494858ca80b1267ceecc811e9badc30a3b92a60e
Bruno Costa <bhcosta90@gmail.com>
2025-03-01 10:17:55 -0300

    [BS-4] feat: update root route to use Livewire component

--- routes/web.php
+++ routes/web.php
@@ -2,8 +2,7 @@
 
 declare(strict_types = 1);
 
+use App\Livewire\Welcome;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', Welcome::class);
